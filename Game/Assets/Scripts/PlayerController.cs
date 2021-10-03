61f8a80b0c6ffe26c54d5d22c0b2a150bf5f3bb1
personkiller <91366243+personkiller@users.noreply.github.com>
2021-10-03 12:14:57 -0700

    Added Shooting

--- Game/Assets/Scripts/PlayerController.cs
+++ Game/Assets/Scripts/PlayerController.cs
@@ -7,23 +7,29 @@ public class PlayerController : MonoBehaviour
     public float speed;
     public float rotationSpeed;
     Vector3 deltaMovement;
+    public Vector3 movementDirection = new Vector3(0, 1);
+    public GameObject bulletPrefab = null;
+    float shotCooldown = 0.6f;
+    public float shotCooldownTimer = 0;
 
-    // Start is called before the first frame update
     void Start()
     {
 
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (shotCooldown > 0)
+        {
+            shotCooldownTimer -= Time.deltaTime;
+        }
         GetInputs();
     }
 
     void GetInputs()
     {
-        deltaMovement.y = Input.GetAxisRaw("Vertical");
-        deltaMovement.x = Input.GetAxisRaw("Horizontal");
+        deltaMovement.y = Input.GetAxis("Vertical");
+        deltaMovement.x = Input.GetAxis("Horizontal");
 
         float magnitude = Mathf.Clamp01(deltaMovement.magnitude);
         deltaMovement.Normalize();
@@ -34,6 +40,21 @@ public class PlayerController : MonoBehaviour
         {
             Quaternion toRotation = Quaternion.LookRotation(Vector3.forward, deltaMovement);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+            UpdateMovementDirection();
         }
+
+        if (shotCooldownTimer <= 0 && Input.GetAxisRaw("Shoot") > 0)
+        {
+            GameObject bulletGo = Instantiate(bulletPrefab, transform.position + movementDirection * 0.5f, transform.rotation);
+            Rigidbody2D bulletRb = bulletGo.GetComponent<Rigidbody2D>();
+            // bulletRb.velocity = Vector2.up * 5;
+            bulletRb.velocity = movementDirection * 5;
+            shotCooldownTimer = shotCooldown;
+        }
+    }
+
+    void UpdateMovementDirection()
+    {
+        movementDirection = new Vector3(Mathf.Round(deltaMovement.x), Mathf.Round(deltaMovement.y));
     }
 }
